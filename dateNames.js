5b690689c82e6e1bfdda4e3f4757ff05ad6d39b2
Nour Habra <NourHBSU@gmail.com>
2022-12-01 22:23:48 +0300

    initial commit

---
+++ dateNames.js
@@ -0,0 +1,15 @@
+module.exports.days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+module.exports.months = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec",
+];
--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-// index.js
-// where your node app starts
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const dateNames = require("./dateNames");
 
-// init project
-var express = require('express');
-var app = express();
+app.use(express.static("public"));
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const PORT = process.env.PORT || 5000;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + "/views/index.html");
+	res.json({ greeting: "hello API" });
 });
 
+// TODO
+// - Handle timestamp and date parameters
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", (req, res) => {
+	let date;
+
+	if (req.params.date[4] == "-") date = new Date(req.params.date);
+	else date = new Date(req.params.date / 1000);
+
+	if (date.toString() == "Invalid Date") res.json({ error: "Invalid Date" });
+
+	let utc = date.toUTCString();
+	let unix = date.getTime() * 1000;
+
+	res.json({
+		unix: unix,
+		utc: utc,
+	});
 });
 
+app.get("/api", (req, res) => {
+	let date = new Date();
 
+	let utc = date.toUTCString();
+	let unix = date.getTime() * 1000;
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	res.json({
+		unix: unix,
+		utc: utc,
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Your app is listening on http://localhost:${PORT}`);
 });
